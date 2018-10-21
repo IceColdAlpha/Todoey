7c318680792b27b8a8ead0a3bfa364d171b26ce5
Jamel Thomas <jamelt@gmail.com>
2018-10-21 08:47:19 -0700

    Persistent data storage/

--- Todoey/Controllers/TodoTableViewController.swift
+++ Todoey/Controllers/TodoTableViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class TodoTableViewController: UITableViewController {
     let defaults = UserDefaults.standard  //Use this for persistent data
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    var itemArray = [Item]()
     
-    var itemArray = ["I win1","I win2","I win3"]
+    
     override func viewDidLoad() {
-        if let items = defaults.array(forKey: "ToDoListArray") as?[String] {
-            itemArray = items
-        }
+        super.viewDidLoad()
+        
+        loadData()
+//        if let items = defaults.array(forKey: "ToDoListArray") as?[Item] {
+//            itemArray = items
+//        }
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -25,9 +31,15 @@ class TodoTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) //
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
@@ -49,9 +61,13 @@ class TodoTableViewController: UITableViewController {
         
         let alert = UIAlertController(title: "Add New Todoey Item", message: "I dunno why she didn't show this ", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray,forKey: "ToDoListArray")
-            self.tableView.reloadData()
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            //self.defaults.set(self.itemArray,forKey: "ToDoListArray")   //Erased this whole line in lecture 227 section 18
+            self.saveData()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
@@ -60,6 +76,30 @@ class TodoTableViewController: UITableViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         
+    }
+    func saveData() {
+        
+        let encoder = PropertyListEncoder()  //to initialize an incoder SEC. 18 Lec. 227  5:34
+        
+        do {
+            let data = try encoder.encode(self.itemArray)  //to use the initialized encoder ##Because this throws an error we need a do catch block and to mark this method with a try.  ##we are in a closure so the itemArray needs to be marked with a self.  ##Now that we have encoded the data we must write the data to a file path.
+            try data.write(to: self.dataFilePath!) //write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+        
+    }
+    func loadData () {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+                self.tableView.reloadData()
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
     
 
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by jamel thomas on 10/14/18.
+//  Copyright © 2018 Jamel Thomas. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
